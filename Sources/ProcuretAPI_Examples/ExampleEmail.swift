6043f0f7323ff94cd6e5a3da3c4980f07029300a
Kayla <kayla.h@procuret.com>
2022-09-12 10:07:36 -0400

    email address example added to example library

---
+++ Sources/ProcuretAPI_Examples/ExampleEmail.swift
@@ -0,0 +1,21 @@
+//
+//  ExampleEmail.swift
+//  
+//
+//  Created by Kayla Hoyet on 9/12/22.
+//
+
+import Foundation
+import ProcuretAPI
+
+extension EmailAddress: ProvidesExample {
+    
+    public static let exampleJson: String = """
+{
+    "confirmation_required": true,
+    "created": "2021-09-06 05:17:19.066145",
+    "email_address": "zac.u+5497854@procuret.com"
+}
+"""
+    
+}
